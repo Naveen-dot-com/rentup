aea56fc9237eeba7d54f742a7f55c06217eea693
Naveen Dubey <n.k.dubey1997@gmail.com>
2026-09-16 19:50:12 +0530

    Update billing.js

--- js/billing.js
+++ js/billing.js
@@ -217,7 +217,8 @@ $(function () {
         headStyles:  { fillColor: [108, 92, 231], textColor: 255, fontStyle: 'bold', fontSize: 10 },
         bodyStyles:  { fontSize: 10, textColor: [50, 50, 50] },
         footStyles:  { fillColor: [240, 240, 250], fontStyle: 'bold', fontSize: 11, textColor: [50, 50, 50] },
-        columnStyles: { 2: { halign: 'center' } },
+        //columnStyles: { 2: { halign: 'center' } },
+        columnStyles: { 2: { halign: 'left' } },
         styles: { cellPadding: 6, font: 'helvetica', overflow: 'linebreak' },
       });
 
